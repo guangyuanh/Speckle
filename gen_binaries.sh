5c7bebf99ffd1cb6ce0f35d3b8565c1555379e53
guangyuanh <thu.guangyuan12@gmail.com>
2017-11-14 17:43:04 -0500

    xalancbmk fixed

--- gen_binaries.sh
+++ gen_binaries.sh
@@ -19,6 +19,8 @@ INPUT_TYPE=test
 
 # the integer set
 BENCHMARKS=(400.perlbench 401.bzip2 403.gcc 429.mcf 445.gobmk 456.hmmer 458.sjeng 462.libquantum 464.h264ref 471.omnetpp 473.astar 483.xalancbmk)
+# Guangyuan for xalancbmk failure Nov14,2017 5pm
+#BENCHMARKS=(400.perlbench 401.bzip2 403.gcc 429.mcf 445.gobmk 456.hmmer 458.sjeng 462.libquantum 464.h264ref 471.omnetpp 473.astar)
 
 # idiomatic parameter and option handling in sh
 compileFlag=false
